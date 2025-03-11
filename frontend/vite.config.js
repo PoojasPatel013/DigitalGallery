b0e0d6ad5c38a3f2a667a2dd90dc2e16e774e537
Poojaspatel013 <poojaspatel1375@gmail.com>
2025-03-11 09:59:11 +0530

    firr

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: import.meta.env.PORT || 5173, // Use Render's port or fallback to 5173
+    host: '0.0.0.0', // Ensure it listens on all interfaces
+  },
+
   plugins: [react(),
     tailwindcss()
   ],
