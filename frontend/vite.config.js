371d28d31fdab495f1bbd91fa4632e543625d72e
Poojaspatel013 <poojaspatel1375@gmail.com>
2025-03-11 10:07:53 +0530

    config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: import.meta.env.PORT || 5173, // Use Render's port or fallback to 5173
-    host: '0.0.0.0', // Ensure it listens on all interfaces
+    port: Number(import.meta.env.PORT) || 5173, // Convert PORT to a number and use 5173 as fallback
+    host: '0.0.0.0', // Allow external access for Render
   },
-
   plugins: [react(),
     tailwindcss()
   ],
